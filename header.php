e9a93a03d9c5f1a473f31efbba0a134e99bd73e0
AmericaPortillo <91654257+AmericaPortillo@users.noreply.github.com>
2021-11-05 14:20:13 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Formularios</title>
+	<link rel="stylesheet" href="estilos-form.css">
+	<link rel="stylesheet" href="estilos.css?v=<?php echo(rand()); ?>" />
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="https://app.facturame.mx/oma/images/logo.jpg" alt="FalconMasters"></a>
+			</div>
+			<nav>
+                <ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="aviones.php">Aviones</a></li>
+					<li><a href="boleto.php">Boleto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
